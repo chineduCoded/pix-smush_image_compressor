eaee41f8873a49f964c35e597cf229fb0f679677
chineduCoded <chinedujohn17@gmail.com>
2023-06-10 16:55:53 +0100

    Upload ui completed

--- ui/src/routes/index.jsx
+++ ui/src/routes/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../styles/home.css"
+import AddIcon from '../components/AddIcon'
+import UploadInCloudIcon from '../components/UploadIcon'
 
 export const HomeScreen = () => {
     return (
@@ -11,15 +13,25 @@ export const HomeScreen = () => {
                 </div>
             </dv>
             <div className='upload-container'>
-                <div className="upload">
-                    <p>Icon</p>
-                    <p>teext</p>
+                <label htmlFor='upload-image' className="upload">
+                    <UploadInCloudIcon color='rgb(24, 92, 77)' size={40} />
+                    <p>Compress jpg, png, webp. Max 5MB</p>
                     <div>
-                        <button>
-                            + Select file
-                        </button>
+                        <div>
+                            <div className='add-image'>
+                                <AddIcon color='rgb(115, 115, 115)' />
+                                <span>Select image</span>
+                            </div>
+                            <input
+                                placeholder='Upload Image'
+                                aria-label='Upload image'
+                                type="file"
+                                name='upload'
+                                id='upload-image'
+                                hidden />
+                        </div>
                     </div>
-                </div>
+                </label>
             </div>
         </div>
     )
